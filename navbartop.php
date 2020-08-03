faf52448f97f42bf8e489ba88b671766b963651d
JohnySR <srjvlr@outlook.com>
2020-08-03 10:39:03 +0530

    Initial commit

---
+++ navbartop.php
@@ -0,0 +1,37 @@
+
+<header style="position:inherit; width:100%; z-index:1" class="shadow">
+
+
+        <div class="row bg-dark text-secondary  " >
+           
+                <div class="col-md-6 col-9 p-3 pl-5  mr-auto">
+                    <h2>JOHNY S R VELLALLOOR</h2>
+                </div>
+                <div class="col-md-6 col-3  my-auto ">
+                    <nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+                  
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+
+                      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto my-auto">
+                          <li class="nav-item active">
+                            <a class="nav-link" href="#Section-Home">Home<span class="sr-only">(current)</span></a>
+                          </li>
+                           <li class="nav-item active">
+                            <a class="nav-link" href="#Section-Services">Services<span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item active">
+                            <a class="nav-link" href="#Section-Experiences">Experiences<span class="sr-only">(current)</span></a>
+                          </li>  
+                          <li class="nav-item active">
+                            <a class="nav-link" href="#Section-ContactUs">Contact<span class="sr-only">(current)</span></a>
+                          </li> 
+                        </ul>
+                      </div>
+                    </nav> 
+               </div>
+                    
+        </div>
+    </header>
